6c513a7429024c1117d818ccc25c9daf5e876ada
Bartosz Arseniuk <arseniuk22864@stud.akademiabialska.pl>
2023-12-08 20:17:54 +0100

    Add server connection

--- app/src/main/java/BimBom/DBI/MainActivity.java
+++ app/src/main/java/BimBom/DBI/MainActivity.java
@@ -1,22 +1,32 @@
 package BimBom.DBI;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+
+import BimBom.DBI.R;
 import pub.devrel.easypermissions.EasyPermissions;
 import pub.devrel.easypermissions.PermissionRequest;
 
@@ -26,10 +36,12 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     private static final int REQUEST_GALLERY = 2;
     private static final int CAMERA_PERMISSION_REQUEST_CODE = 1;
     private static final int GALLERY_PERMISSION_REQUEST_CODE = 2;
+    private static final int REQUEST_STORAGE_PERMISSION = 3;
 
     private ImageView imageView;
     private Button btnCamera;
     private Button btnGallery;
+    private Button btnUpload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         imageView = findViewById(R.id.imageView);
         btnCamera = findViewById(R.id.btnCamera);
         btnGallery = findViewById(R.id.btnGallery);
+        btnUpload = findViewById(R.id.btnUpload);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,11 +63,18 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         btnGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (EasyPermissions.hasPermissions(MainActivity.this, Manifest.permission.CAMERA)) {
+                if (EasyPermissions.hasPermissions(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
                     openGallery();
                 } else {
-                    Toast.makeText(MainActivity.this, "Nie można otworzyć galerii bez uprawnień do aparatu", Toast.LENGTH_SHORT).show();
+                    requestStoragePermission();
                 }
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Tutaj możesz dodać logikę dla przesyłania zdjęcia na serwer
             }
         });
     }
@@ -85,29 +105,20 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
-    }
-
-    @Override
     public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
-        // Użytkownik przyznał uprawnienia
-        Log.d("MainActivity", "Uprawnienia przyznane");
         if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
             openCamera();
         } else if (requestCode == GALLERY_PERMISSION_REQUEST_CODE) {
             if (EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
                 openGallery();
             } else {
-                Toast.makeText(this, "Nie masz uprawnień do odczytu z pamięci zewnętrznej", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Brak uprawnień do odczytu z pamięci zewnętrznej", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     @Override
     public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
-        // Użytkownik odmówił uprawnień
         if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
             Toast.makeText(this, "Nie można otworzyć aparatu bez uprawnień", Toast.LENGTH_SHORT).show();
         } else if (requestCode == GALLERY_PERMISSION_REQUEST_CODE) {
@@ -121,21 +132,59 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == REQUEST_CAMERA && data != null && data.getExtras() != null) {
-                // Zdjęcie z aparatu
                 Bitmap photo = (Bitmap) data.getExtras().get("data");
                 if (photo != null) {
                     imageView.setImageBitmap(photo);
+                    String base64Image = convertBitmapToBase64(photo);
+                    // Tutaj możesz wykorzystać base64Image do wysłania na serwer
                 }
             } else if (requestCode == REQUEST_GALLERY && data != null) {
-                // Zdjęcie z galerii
                 Uri selectedImageUri = data.getData();
                 try {
                     Bitmap photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
                     imageView.setImageBitmap(photo);
+                    String base64Image = convertBitmapToBase64(photo);
+                    // Tutaj możesz wykorzystać base64Image do wysłania na serwer
                 } catch (IOException e) {
                     e.printStackTrace();
                     Log.e("MainActivity", "Błąd przy wczytywaniu zdjęcia z galerii", e);
                 }
+            }
+        }
+    }
+
+    private String convertBitmapToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    openGallery();
+                } else {
+                    Toast.makeText(this, "Brak uprawnień do odczytu z pamięci zewnętrznej", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    private void requestStoragePermission() {
+        requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_STORAGE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            } else {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.fromParts("package", getPackageName(), null));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
             }
         }
     }
--- app/src/main/java/BimBom/DBI/ViewModel/PhotoViewModel.java
+++ app/src/main/java/BimBom/DBI/ViewModel/PhotoViewModel.java
@@ -1,22 +1,93 @@
-package BimBom.DBI.ViewModel;
+package BimBom.DBI;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.provider.MediaStore;
-
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
+import java.io.ByteArrayOutputStream;
+
+import BimBom.DBI.ApiService.ApiService;
 import BimBom.DBI.Model.PhotoModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-public class PhotoViewModel {
+public class PhotoViewModel extends ViewModel {
     private MutableLiveData<PhotoModel> selectedPhoto = new MutableLiveData<>();
+    private MutableLiveData<String> uploadStatus = new MutableLiveData<>();
 
-    public LiveData<PhotoModel> getSelectedPhoto() {
+    public MutableLiveData<String> getUploadStatus() {
+        return uploadStatus;
+    }
+
+    public MutableLiveData<PhotoModel> getSelectedPhoto() {
+        if (selectedPhoto.getValue() == null) {
+            // Jeśli selectedPhoto jest null, zainicjuj nowy obiekt PhotoModel
+            selectedPhoto.setValue(new PhotoModel(""));
+        }
         return selectedPhoto;
     }
 
     public void setPhoto(String imagePath) {
-        selectedPhoto.setValue(new PhotoModel(imagePath));
+        // Convert the image to Base64
+        String base64Image = convertImageToBase64(imagePath);
+
+        // Send the Base64 image to the REST API
+        sendImageToApi(base64Image);
+    }
+
+    private String convertImageToBase64(String imagePath) {
+        // Convert the image to byte array
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        // Load the image from file
+        Bitmap photo = BitmapFactory.decodeFile(imagePath);
+
+        if (photo != null) {
+            // Convert byte array to Base64
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            byte[] byteArray = byteArrayOutputStream.toByteArray();
+            return Base64.encodeToString(byteArray, Base64.DEFAULT);
+        } else {
+            // Handle the case when the image couldn't be loaded
+            // For now, return an empty string, but you might want to handle it differently based on your requirements.
+            return "";
+        }
+    }
+
+    private void sendImageToApi(String base64Image) {
+        // Retrofit setup
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://localhost:7219/swagger/index.html/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // Create API service
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        // Create PhotoModel with Base64 image
+        PhotoModel photoModel = new PhotoModel(base64Image);
+
+        // Send the photo to the API
+        Call<String> call = apiService.uploadPhoto(photoModel);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()) {
+                    uploadStatus.setValue("Zdjęcie zostało pomyślnie przesłane");
+                } else {
+                    uploadStatus.setValue("Błąd podczas przesyłania zdjęcia");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                uploadStatus.setValue("Błąd podczas przesyłania zdjęcia: " + t.getMessage());
+            }
+        });
     }
 }
