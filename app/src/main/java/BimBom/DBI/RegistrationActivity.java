a52d531b93672e237e30fff9bedbfe65fe17c4cb
Maties90 <mmatejko62@gmial.com>
2024-01-08 19:48:20 +0100

    add registration feature

--- app/src/main/java/BimBom/DBI/RegistrationActivity.java
+++ app/src/main/java/BimBom/DBI/RegistrationActivity.java
@@ -1,14 +1,78 @@
 package BimBom.DBI;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private EditText emailET, passwordET;
+    private Button btnSign,btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailET = findViewById(R.id.etEmail);
+        passwordET = findViewById(R.id.etPassword);
+        btnSign= findViewById(R.id.btnSingUp);
+        btnBack= findViewById(R.id.btnBack);
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register(v);
+            }
+
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+        });
+    }
+
+    public void register(View view) {
+        String email = emailET.getText().toString().trim();
+        String password = passwordET.getText().toString().trim();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this, "Rejestracja udana", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, "Rejestracja nieudana", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
+
+
