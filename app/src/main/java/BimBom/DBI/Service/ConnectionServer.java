9da24256c364919d5fb679135d56147fb9f6cfa7
Bartosz Arseniuk <arseniuk22864@stud.akademiabialska.pl>
2024-02-19 20:35:33 +0100

    Reduce timeout for server response

--- app/src/main/java/BimBom/DBI/Service/ConnectionServer.java
+++ app/src/main/java/BimBom/DBI/Service/ConnectionServer.java
@@ -23,8 +23,8 @@ public class ConnectionServer {
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
         OkHttpClient.Builder clientBuilder = UnsafeOkHttpClient.getUnsafeOkHttpClient().newBuilder();
-        clientBuilder.readTimeout(200, TimeUnit.SECONDS);
-        clientBuilder.writeTimeout(200, TimeUnit.SECONDS);
+        clientBuilder.readTimeout(100, TimeUnit.SECONDS);
+        clientBuilder.writeTimeout(100, TimeUnit.SECONDS);
 
         JwtInterceptor jwtInterceptor = new JwtInterceptor(context);
         clientBuilder.addInterceptor(jwtInterceptor);
